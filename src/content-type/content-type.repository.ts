4410ab7fa8449ed231f7ca44f89a34a63beb2a7e
Jeppe Smith <mail@jeppesmith.dk>
2019-05-19 11:10:21 +0200

    remove content-type

--- src/content-type/content-type.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { ContentType } from './content-type.entity'
-
-@EntityRepository(ContentType)
-export class ContentTypeRepository extends Repository<ContentType> {}
